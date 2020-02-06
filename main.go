d3dc5adf0d0d20701b984abcf0dfeba2514fdcf9
Matthew Riley <mdriley@gmail.com>
2020-02-06 01:19:35 -0800

    Implement very simple port forwarding
    Allows `./testssh -W neverssl.com:80`

--- main.go
+++ main.go
@@ -3,32 +3,13 @@ package main
 import (
 	"crypto/ed25519"
 	"fmt"
+	"io"
 	"log"
 	"net"
 
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/terminal"
 )
-
-// https://tools.ietf.org/html/rfc4254#section-7.2 shows "direct-tcpip" fields
-// https://tools.ietf.org/html/rfc4254#section-5.1 shows which are "extra"
-
-type DirectTcpipChannelMsgExtraData struct {
-	Host           string
-	Port           uint32
-	OriginatorIp   string
-	OriginatorPort uint32
-}
-
-func printDirectTcpipRequest(b []byte) {
-	var s DirectTcpipChannelMsgExtraData
-	err := ssh.Unmarshal(b, &s)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%+v\n", s)
-}
 
 // https://tools.ietf.org/html/rfc4254#section-7.1
 type TcpipForwardRequestPayload struct {
@@ -108,45 +89,119 @@ func handleConnection(netconn net.Conn, config *ssh.ServerConfig) {
 
 	// Service the channel of incoming channel requests
 	for proposedChannel := range channels {
-		// Channels have a type, depending on the application level
-		// protocol intended. In the case of a shell, the type is
-		// "session" and ServerShell may be used to present a simple
-		// terminal interface.
 		t := proposedChannel.ChannelType()
 		fmt.Printf("New proposed channel of type %v\n", t)
-		if t == "direct-tcpip" {
-			printDirectTcpipRequest(proposedChannel.ExtraData())
-		} else if proposedChannel.ChannelType() != "session" {
+
+		switch t {
+		case "direct-tcpip":
+			handleDirectTcpipChannelRequest(sshconn, proposedChannel)
+		case "session":
+			handleSessionChannelRequest(sshconn, proposedChannel)
+
+		default:
 			proposedChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
-			continue
 		}
-		channel, channelRequests, err := proposedChannel.Accept()
-		if err != nil {
-			log.Fatalf("Could not accept channel: %v", err)
-		}
-
-		// Sessions have out-of-band requests such as "shell",
-		// "pty-req" and "env".  Here we handle only the
-		// "shell" request.
-		go func(in <-chan *ssh.Request) {
-			for req := range in {
-				req.Reply(req.Type == "shell", nil)
-			}
-		}(channelRequests)
-
-		term := terminal.NewTerminal(channel, "> ")
-
-		go func() {
-			defer channel.Close()
-			for {
-				line, err := term.ReadLine()
-				if err != nil {
-					break
-				}
-				fmt.Println(line)
-			}
-		}()
 	}
+}
+
+func handleSessionChannelRequest(c ssh.Conn, req ssh.NewChannel) error {
+	channel, channelRequests, err := req.Accept()
+	if err != nil {
+		log.Fatalf("Could not accept channel: %v", err)
+	}
+
+	// Sessions have out-of-band requests such as "shell",
+	// "pty-req" and "env".  Here we handle only the
+	// "shell" request.
+	go func(in <-chan *ssh.Request) {
+		for req := range in {
+			req.Reply(req.Type == "shell", nil)
+		}
+	}(channelRequests)
+
+	term := terminal.NewTerminal(channel, "> ")
+
+	go func() {
+		defer channel.Close()
+		for {
+			line, err := term.ReadLine()
+			if err != nil {
+				break
+			}
+			fmt.Println(line)
+		}
+	}()
+
+	return nil
+}
+
+func copyBothWays(lhs, rhs io.ReadWriteCloser) error {
+	errs := make(chan error)
+
+	go func() {
+		_, err := io.Copy(rhs, lhs)
+		errs <- err
+	}()
+
+	go func() {
+		_, err := io.Copy(lhs, rhs)
+		errs <- err
+	}()
+
+	defer lhs.Close()
+	defer rhs.Close()
+
+	// TODO: the goal here is to properly handle half-closed connections where
+	// one side has closed its sending side but is still willing to receive.
+	// However, the net effect is that the connection remains open even after
+	// one party is *entirely gone*.
+	//
+	// Coarse solution is to stop all copies when one hits an error.
+	// Coordinate with a channel.
+	//
+	// Seems like there's a stickier problem here around how to deal with
+	// idle connections overall.
+	//
+	// Is there a way to determine a side is dead (and not just half-closed)
+	// without trying to write to it?
+	if err := <-errs; err != nil {
+		return err
+	}
+	return <-errs
+}
+
+// https://tools.ietf.org/html/rfc4254#section-7.2 shows "direct-tcpip" fields
+// https://tools.ietf.org/html/rfc4254#section-5.1 shows which are "extra"
+
+type DirectTcpipChannelMsgExtraData struct {
+	Host           string
+	Port           uint32
+	OriginatorIP   string
+	OriginatorPort uint32
+}
+
+func handleDirectTcpipChannelRequest(c ssh.Conn, req ssh.NewChannel) error {
+	var data DirectTcpipChannelMsgExtraData
+	err := ssh.Unmarshal(req.ExtraData(), &data)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%+v\n", data)
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%v:%v", data.Host, data.Port))
+	if err != nil {
+		req.Reject(ssh.ConnectionFailed, "")
+		return err
+	}
+
+	channel, requests, err := req.Accept()
+	go ssh.DiscardRequests(requests)
+	go func() {
+		err := copyBothWays(channel, conn)
+		fmt.Printf("copyBothWays returned %v\n", err)
+	}()
+
+	return nil
 }
 
 func main() {
